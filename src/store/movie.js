862ce456a36d944aeb061ce8c515b418dd8d691c
kjh302903 <kjh302903@gmail.com>
2023-02-16 16:42:50 +0900

    error 예외처리 추가

--- src/store/movie.js
+++ src/store/movie.js
@@ -4,20 +4,35 @@ const store = new Store({
     searchText: '',
     page: 1,
     pageMax: 1,
-    movies: []
+    movies: [],
+    loading: false,
+    message: 'Search for the movie title!'
 })
 
 export default store
 export const searchMovies = async page => {
+    store.state.loading = true
     store.state.page = page
     if(page ===1) {
         store.state.movies = []
+        store.state.message = ''
     }
-    const res = await fetch(`https://omdbapi.com/?apikey=7035c60c&s=${store.state.searchText}&page=${store.state.page}`)
-    const { Search, totalResults } = await res.json()
-    store.state.movies = [
-        ...store.state.movies,
-        ...Search
-    ]
-    store.state.pageMax = Math.ceil(Number(totalResults) / 10) 
+    try {
+        const res = await fetch(`https://omdbapi.com/?apikey=7035c60c&s=${store.state.searchText}&page=${store.state.page}`)
+        const { Search, totalResults, Response, Error } = await res.json()
+        if (Response === 'True') {
+            store.state.movies = [
+                ...store.state.movies,
+                ...Search
+            ]
+            store.state.pageMax = Math.ceil(Number(totalResults) / 10)
+        }
+        else {
+            store.state.message = Error
+        }
+    } catch (error) {
+        console.log(error)
+    } finally {
+        store.state.loading = false
+    }
 }
