9ef08adb5b3fe57af8749f03ac38a2b011b5b950
kjh302903 <kjh302903@gmail.com>
2023-02-18 14:58:01 +0900

    About page 추가

---
+++ src/store/about.js
@@ -0,0 +1,9 @@
+import { Store } from "../core/heropy"
+
+export default new Store({
+    photo: 'https://heropy.blog/css/images/logo.png',
+    name: 'kjh302903 / KangJuHo',
+    email: 'kjh302903@gmail.com',
+    github: 'https://github.com/kjh302903',
+    repository: 'https://github.com/kjh302903/movie-search-app'
+})
