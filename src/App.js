56cdcda67350e24d8a223977b86379da53d28729
kjh302903 <kjh302903@gmail.com>
2023-02-18 13:30:47 +0900

    TheHeader 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { Component } from "./core/heropy"
+import TheHeader from "./components/TheHeader"
 
 export default class App extends Component {
     render() {
         const routerView = document.createElement('router-view')
+        const theHeader = new TheHeader().el
 
-        this.el.append(routerView)
+        this.el.append(
+            theHeader,
+            routerView)
     }
 }
